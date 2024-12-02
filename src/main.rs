740c6bba0a93f0f1600d33f300cbce0346eff316
markl <mark.lambert.wtap@gmail.com>
2024-12-02 15:34:59 -0500

    added tests for dice_totals module

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("dice roller");
     println!("How many dice do you need to roll?");
     match io::stdin().read_line(&mut input).unwrap() {
-        none => println!("You need to roll?") };
+        _none => println!("You need to roll?") };
 
     let dice_number :i8 = input.trim().parse().unwrap();
     let mut dice_list: Vec<Dice> = Vec::new();
@@ -16,7 +16,7 @@ fn main() {
     for i in 0..dice_number {
         println!("how many side does dice {} have", i+1);
         let mut sides = String::new();
-        match io::stdin().read_line(&mut sides).unwrap() { none => println!("You need to enter a value?") };
+        match io::stdin().read_line(&mut sides).unwrap() { _none => println!("You need to enter a value?") };
         let side :i8 = sides.trim().parse().unwrap();
         &dice_list.push(Dice::new(side));
     }
@@ -27,7 +27,7 @@ fn main() {
         &outcomes.push(Dice::roll_dice(die));
     }
 
-    let mut outcome_string = Dice::calculate_outcomes(&outcomes);
+    let outcome_string = Dice::calculate_outcomes(&outcomes);
 
 
 
@@ -65,9 +65,9 @@ pub mod dice_tools{
             total
         }
 
-        pub fn calculate_outcomes(outcomes:&Vec<i8>)-> String{
+        pub fn calculate_outcomes(outcomes:&Vec<i8>)-> String {
             let mut output = String::new();
-            for item in &outcomes {
+            for item in outcomes {
 
                 if !outcomes.last().eq(&Some(&item)) {
                     output += &item.to_string();
@@ -78,6 +78,31 @@ pub mod dice_tools{
                 }
 
             }
+            output
+        }
+    }
+
+    mod dice_tests{
+        use super::*;
+
+        #[test]
+        pub fn test_total(){
+            let mut numbers:Vec<i8> = Vec::new();
+            numbers.push(1);
+            numbers.push(2);
+            numbers.push(3);
+
+            assert_eq!(Dice::total(numbers), 6);
+
+        }
+
+        #[test]
+        pub fn test_calculate_outcomes(){
+            let mut numbers:Vec<i8> = Vec::new();
+            numbers.push(1);
+            numbers.push(2);
+            numbers.push(3);
+            assert_eq!(Dice::calculate_outcomes(&numbers), "1, 2, and 3");
         }
     }
 
